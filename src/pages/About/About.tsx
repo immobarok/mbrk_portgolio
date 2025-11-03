294a74707de689a539ca1e902317f67b6464edab
immobarok <immobarokali2001@gmail.com>
2025-11-04 03:59:07 +0600

    test ci cd

--- src/pages/About/About.tsx
+++ src/pages/About/About.tsx
@@ -2,8 +2,10 @@ const About = () => {
   return (
     <main id="about" className="min-h-screen w-full">
       <h1>About Page</h1>
-      <p>This is mobarok from bangladesh</p>
-      <h1>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Consequatur laboriosam corrupti perferendis provident fugit. Reprehenderit omnis necessitatibus amet aliquid adipisci esse voluptates itaque ratione neque qui minus, quia rerum quisquam maxime sint cumque eius. Aliquam consequatur cumque atque repellendus mollitia assumenda recusandae temporibus cupiditate ut dolor magni natus laudantium eaque, accusamus numquam aperiam perspiciatis doloremque deserunt obcaecati, esse accusantium harum voluptas modi blanditiis. Suscipit voluptates soluta numquam blanditiis voluptatem. Fugiat dolor laudantium, necessitatibus inventore sunt fuga, voluptatibus eligendi nesciunt aspernatur eius modi libero ab officiis unde nobis aut, consectetur omnis accusamus nihil exercitationem illo enim atque tempora maiores! Voluptatibus, impedit.</h1>
+      <h1 className="text-4xl font-bold mt-20 text-center max-w-3xl mx-auto">
+        We combine years of web design and branding expertise to craft
+        meaningful, story-driven experiences
+      </h1>
     </main>
   );
 };
